7b31177b5f518ce833c907459c54d9b92795b9f8
KeiichiAoyama <zinedinexaviorzethro17@email.com>
2023-08-13 17:31:46 +0700

    First transfer UwU

---
+++ app/src/main/java/Prak9_00000067586/com/MainActivity.java
@@ -0,0 +1,14 @@
+package Prak9_00000067586.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
